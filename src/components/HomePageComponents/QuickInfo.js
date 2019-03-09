310dd6f0f65c711b6533ae06c33269df05a081f0
Param Saluja <salujaparam@gmail.com>
2019-03-09 07:10:04 +0530

    added content to home about contact and menu pages

---
+++ src/components/HomePageComponents/QuickInfo.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react"
+import { Section, Title, SectionButton } from "../../utils"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import { styles } from "../../utils"
+
+export default class QuickInfo extends Component {
+  render() {
+    return (
+      <Section>
+        <Title message="let us tell you" title="our mission" />
+        <QuickInfoWrapper>
+          <p className="text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <Link to="/about/" style={{ textDecoration: "none" }}>
+            <SectionButton style={{ margin: "2rem auto" }}>about</SectionButton>
+          </Link>
+        </QuickInfoWrapper>
+      </Section>
+    )
+  }
+}
+
+const QuickInfoWrapper = styled.div`
+  width: 90%;
+  margin: 2rem auto;
+  .text {
+    line-height: 2rem;
+    color: ${styles.colors.mainGrey};
+    word-spacing: 0.2rem;
+  }
+  @media (min-width: 768px) {
+    width: 70%;
+  }
+  @media (min-width: 992px) {
+    width: 60%;
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,19 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { HomeHeader, Banner, BannerButton } from "../utils"
+import img from "../images/bcg/homeBcg.jpeg"
+import QuickInfo from "../components/HomePageComponents/QuickInfo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h3>Home Page</h3>
+    <HomeHeader img={img}>
+      <Banner title="eatery" subtitle="14 church st, ashok nagar, bengaluru">
+        <BannerButton style={{ margin: "2rem auto" }}>Menu</BannerButton>
+      </Banner>
+    </HomeHeader>
+    <QuickInfo />
   </Layout>
 )
 
